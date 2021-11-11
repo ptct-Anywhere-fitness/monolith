09d7362264df088ba2c5175f8ad36c0a6327f74e
JoshuaHolloway <jhollow6@asu.edu>
2021-11-11 05:56:16 -0600

    cleaned up further

--- client/components/layout.js
+++ client/components/layout.js
@@ -23,68 +23,53 @@ export default function Layout(props) {
     // Logged-in
     if (props.user.role === 'admin') {
       menuItems = [
-        {
-          text: 'Home',
-          icon: (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              viewBox='0 0 16 16'
-            >
-              <path
-                fillRule='evenodd'
-                d='M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z'
-              />
-              <path
-                fillRule='evenodd'
-                d='M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z'
-              />
-            </svg>
-          ),
-          path: '/dashboard-admin',
-        },
+        // {
+        //   text: 'Home',
+        //   icon: (
+        //     <svg
+        //       xmlns='http://www.w3.org/2000/svg'
+        //       width='16'
+        //       height='16'
+        //       fill='currentColor'
+        //       viewBox='0 0 16 16'
+        //     >
+        //       <path
+        //         fillRule='evenodd'
+        //         d='M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z'
+        //       />
+        //       <path
+        //         fillRule='evenodd'
+        //         d='M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z'
+        //       />
+        //     </svg>
+        //   ),
+        //   path: '/dashboard-admin',
+        // },
       ];
     } else {
       menuItems = [
-        {
-          text: 'Home',
-          icon: (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              viewBox='0 0 16 16'
-            >
-              <path
-                fillRule='evenodd'
-                d='M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z'
-              />
-              <path
-                fillRule='evenodd'
-                d='M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z'
-              />
-            </svg>
-          ),
-          path: '/dashboard-customer',
-        },
-        {
-          text: 'Cart',
-          icon: (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              viewBox='0 0 16 16'
-            >
-              <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z' />
-            </svg>
-          ),
-          path: '/cart',
-        },
+        // {
+        //   text: 'Home',
+        //   icon: (
+        //     <svg
+        //       xmlns='http://www.w3.org/2000/svg'
+        //       width='16'
+        //       height='16'
+        //       fill='currentColor'
+        //       viewBox='0 0 16 16'
+        //     >
+        //       <path
+        //         fillRule='evenodd'
+        //         d='M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z'
+        //       />
+        //       <path
+        //         fillRule='evenodd'
+        //         d='M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z'
+        //       />
+        //     </svg>
+        //   ),
+        //   path: '/dashboard-customer',
+        // },
       ];
     }
   } else {
@@ -107,7 +92,7 @@ export default function Layout(props) {
       {/* navbar */}
       <Navbar bg='dark' variant='dark'>
         <Container>
-          <Navbar.Brand href='#home'>Anywhere Fitness</Navbar.Brand>
+          <Navbar.Brand href='/'>Anywhere Fitness</Navbar.Brand>
 
           <Nav className='me-auto'>
             {menuItems.map((item) => (
