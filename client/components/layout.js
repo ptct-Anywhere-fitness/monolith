4bea044026f574c3c535915cbacecac8b18ca00e
JoshuaHolloway <jhollow6@asu.edu>
2021-11-11 05:19:07 -0600

    redirecting / to either /auth, /dashboard-customer, or /dashboard-admin (depending on authCtx.isLoggedIn and authCtx.user.role

--- client/components/layout.js
+++ client/components/layout.js
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
-// import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import Button from 'react-bootstrap/Button';
@@ -139,62 +138,7 @@ export default function Layout(props) {
     }
   } else {
     // Not logged-in
-    menuItems = [
-      {
-        text: 'Home',
-        icon: (
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-          >
-            <path
-              fillRule='evenodd'
-              d='M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z'
-            />
-            <path
-              fillRule='evenodd'
-              d='M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z'
-            />
-          </svg>
-        ),
-        path: '/',
-      },
-
-      {
-        text: 'Log In',
-        icon: (
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-          >
-            <path d='M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z' />
-            <path d='M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z' />
-          </svg>
-        ),
-        path: '/auth',
-      },
-      {
-        text: 'Cart',
-        icon: (
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-          >
-            <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z' />
-          </svg>
-        ),
-        path: '/cart',
-      },
-    ];
+    menuItems = [];
   }
 
   // --------------------------------------------
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,36 @@
-import NavBar from '../components/navbar';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+
+import { AuthContext } from '../context/auth-context';
 
 // ==============================================
 
 export default function HomePage() {
+  // --------------------------------------------
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  // --------------------------------------------
+
+  const router = useRouter();
+  const authCtx = useContext(AuthContext);
+
+  // --------------------------------------------
+
+  if (authCtx.isLoggedIn) {
+    if (authCtx.user.role === 'admin') {
+      router.push('/dashboard-admin');
+    } else {
+      router.push('/dashboard-customer');
+    }
+  } else if (mounted) {
+    router.push('/auth');
+  }
+
   // --------------------------------------------
 
   return (
