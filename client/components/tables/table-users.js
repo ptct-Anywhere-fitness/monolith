b48090efc06797dc7a2fb3b6cc3a5857b1edc2d2
JoshuaHolloway <jhollow6@asu.edu>
2021-11-12 04:28:41 -0600

    table for users

--- client/components/tables/table-users.js
+++ client/components/tables/table-users.js
@@ -1,50 +1,61 @@
 import * as React from 'react';
 
-function createData(username, user_id, role, password) {
-  return { username, user_id, role, password };
-}
+import Table from 'react-bootstrap/Table';
 
 // ==============================================
 
 export default function TableUsers({ users }) {
-  const rows = users.map((user) => {
-    return createData(
-      user.username,
-      user.user_id,
-      user.role,
-      user.password,
-      user.first_name,
-      user.last_name
-    );
-  }, []);
+  // --------------------------------------------
 
   return (
     <>
-      <table
-        style={{
-          fontFamily: 'arial, sans-serif',
-          borderCollapse: 'collapse',
-          width: '100%',
-          maxHeight: '260px',
-          overflowY: 'scroll',
-        }}
+      <Table
+        striped
+        bordered
+        hover
+        variant='dark'
+        responsive
+        style={{ maxHeight: '260px', overflowY: 'scroll' }}
       >
-        <tr>
-          <th></th>
-          <th>ID</th>
-          <th>Role</th>
-          <th>Password</th>
-        </tr>
-        {rows &&
-          rows.map((row, idx) => (
-            <tr key={idx}>
-              <td>+</td>
-              <td>{row?.user_id}</td>
-              <td>{row?.role}</td>
-              <td>{row?.password}</td>
-            </tr>
-          ))}
-      </table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>ID</th>
+            <th>User Name</th>
+            <th>Role</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users &&
+            users.map((user, idx) => {
+              return (
+                <tr key={idx}>
+                  <td>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      width='16'
+                      height='16'
+                      fill='currentColor'
+                      viewBox='0 0 16 16'
+                    >
+                      <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z' />
+                      <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z' />
+                    </svg>
+                  </td>
+                  <td>{user?.user_id}</td>
+                  <td>{user?.username}</td>
+                  <td>{user?.role}</td>
+                  <td>{user?.first_name}</td>
+                  <td>{user?.last_name}</td>
+                  <td>{user?.password}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
     </>
   );
 }
