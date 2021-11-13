d73718779de960181eb95afbd0456ea541759de9
JoshuaHolloway <jhollow6@asu.edu>
2021-11-13 12:23:37 -0600

    added animation on notification bar

--- client/components/notification/notification.js
+++ client/components/notification/notification.js
@@ -1,4 +1,6 @@
-import { useContext } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
+
+import { gsap } from 'gsap';
 
 import classes from './notification.module.scss';
 import NotificationContext from '../../context/notification-context';
@@ -6,28 +8,68 @@ import NotificationContext from '../../context/notification-context';
 // ==============================================
 
 export default function Notification(props) {
+  // --------------------------------------------
+
   const notificationCtx = useContext(NotificationContext);
+
+  // --------------------------------------------
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  // --------------------------------------------
+
+  const div_ref = useRef();
+  const timeline_ref = useRef();
+
+  useEffect(() => {
+    if (mounted && notificationCtx.notification !== null) {
+      timeline_ref.current = gsap.timeline().to(div_ref.current, {
+        duration: 0.2,
+        yPercent: '-100',
+        onReverseComplete: () => {
+          // setCssClassList(`${css.backdrop} ${css.hide}`);
+        },
+      });
+    } else if (mounted && notificationCtx.notification.notification === null) {
+      if (timeline_ref) {
+        timeline_ref.current.reverse();
+      }
+    }
+  }, [notificationCtx.notification]);
+
+  // --------------------------------------------
 
   const { title, message, status } = props;
 
-  let statusClasses = '';
+  // --------------------------------------------
 
+  let statusClasses = '';
   if (status === 'success') {
     statusClasses = classes.success;
   }
-
   if (status === 'error') {
     statusClasses = classes.error;
   }
-
   if (status === 'pending') {
     statusClasses = classes.pending;
   }
 
+  // --------------------------------------------
+
   const activeClasses = `${classes.notification} ${statusClasses}`;
 
+  // --------------------------------------------
+
   return (
-    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
+    <div
+      ref={div_ref}
+      className={activeClasses}
+      onClick={notificationCtx.hideNotification}
+    >
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
