49788abcfb54f21760c86c955cf5de3c4b212789
JoshuaHolloway <jhollow6@asu.edu>
2021-11-14 12:22:49 -0600

    graphing two clicks on slider

--- client/pages/dashboard-customer.js
+++ client/pages/dashboard-customer.js
@@ -9,11 +9,11 @@ import Cart from '../components/cart';
 import Calendar from '../components/calendar/Calendar';
 import RangeSlider from '../components/range-slider/range-slider';
 
+// import Canvas_1 from '../components/canvas/canvas-1/canvas';
+import Canvas_2 from '../components/canvas/canvas-2/canvas';
+
 // import { AuthContext } from '../context/auth-context';
 // import { LoadingContext } from '../context/loading-context';
-
-import { element_geometry, viewport_geometry } from '../helpers/geometry.js';
-
 
 // ==============================================
 
@@ -90,150 +90,11 @@ export default function CustomerDashboardPage() {
 
   // --------------------------------------------
 
-  const canvas_ref = useRef();
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-    return () => setMounted(false);
-  }, []);
-
-  useEffect(() => {
-    if(mounted) {
-
-      
-      const ctx = canvas_ref.current.getContext('2d');
-      console.log(ctx);
-      ctx.clearRect(0, 0, canvas_ref.current.width, canvas_ref.current.height);
-
-      const canvas_basics = () => {
-        // fillRect();
-        ctx.fillStyle='red';
-        ctx.fillRect(20, 20, 150, 100);
-
-        // strokeRect()
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = 'green';
-        ctx.strokeRect(100, 200, 150, 100);
-
-        // clearRect()
-        ctx.clearRect(25, 25, 140, 90);
-
-        // fillext()
-        ctx.font = '30px Arial';
-        ctx.fillStyle = 'purple';
-        ctx.fillText('hello world', 400, 80);
-      };
-      // canvas_basics();
-
-      const animation = () => {
-        const circle = {
-          x: 200,
-          y: 200,
-          size: 30,
-          dx: 5,
-          dy: 4
-        };
-
-        function drawCircle() {
-          ctx.beginPath();
-          ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
-          ctx.fillStyle = 'purple';
-          ctx.fill();
-        }
-
-        (function update() {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-          drawCircle();
-
-          // change position
-          circle.x += circle.dx;
-          circle.y += circle.dy;
-
-          // Detect side walls
-          if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
-            circle.dx *= -1;
-          }
-
-          // Detect top and bottom walls
-          if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
-            circle.dy *= -1;
-          }
-
-          requestAnimationFrame(update);
-        })();
-      };
-      // animation();
-
-      const draw_slider = () => {
-
-
-        function drawCircle(circle) {
-          ctx.beginPath();
-          ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
-          ctx.fillStyle = 'purple';
-          ctx.fill();
-        }
-
-        drawCircle({
-          x: 200,
-          y: 200,
-          size: 30,
-          dx: 5,
-          dy: 40
-        });
-      };
-      // draw_slider();
-
-    }
-  }, [mounted]);
-
-  // --------------------------------------------
 
   return (
     <>
-      <canvas ref={canvas_ref} id="canvas" width="600px" height="600px" 
-        style={{background: 'lightgray', borderRadius: '5px', 
-          // position: 'absolute', top: 0, left: 0
-        }} onClick={(e) => {
-        function get_mouse_coordinates(event) {
-          const [x, y] = [event.clientX, event.clientY];
-          return [x, y];
-        }
-        
-        console.log('clicked');
-        const { x0, y0, x1, y1, w, h }= element_geometry(canvas_ref.current);
-        console.log('x0: ', x0);
-        console.log(`(x1, y1)  :  (${x1}, ${y1})`);
-
-        // Step 0: Clear canvas
-        const ctx = canvas_ref.current.getContext('2d');
-        console.log(ctx);
-        ctx.clearRect(0, 0, canvas_ref.current.width, canvas_ref.current.height);
-
-        // Step 1: Get mouse coordinates
-        const [x, y] = get_mouse_coordinates(e);
-        console.log(`(x, y)  :  (${x}, ${y})`);
-
-        // Step 2: Draw circle at location clicked
-        function drawCircle(circle) {
-          ctx.beginPath();
-          ctx.arc(circle.x - x1, circle.y - y1, circle.size, 0, Math.PI * 2);
-          // ctx.arc(circle.x - 25, circle.y-100, circle.size, 0, Math.PI * 2);
-          ctx.fillStyle = 'purple';
-          ctx.fill();
-        }
-
-        drawCircle({
-          x,
-          y,
-          size: 30,
-          // dx: 5,
-          // dy: 40
-        });
-
-  
-      }}></canvas>
+      {/* <Canvas_1 /> */}
+      <Canvas_2 />
     </>
   );
 }
