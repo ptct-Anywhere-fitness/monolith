bb004831361378c8bb7c49af21db8652bdc0c53f
JoshuaHolloway <jhollow6@asu.edu>
2021-11-15 11:52:58 -0600

    mapped query to knex command

--- api/orders/orders-model.js
+++ api/orders/orders-model.js
@@ -22,13 +22,19 @@ function getUsersOrders() {
   // 	u.username as username
   // from users as u -- from left table
   // left join orders as o on u.user_id = o.user_id_fk; -- join tables
-  return db.raw(`
-    select
-      o.total as total,
-      u.username as username
-    from users as u
-    left join orders as o on u.user_id = o.user_id_fk;
-  `);
+  //
+  // return db.raw(`
+  //   select
+  //     o.total as total,
+  //     u.username as username
+  //   from users as u
+  //   left join orders as o on u.user_id = o.user_id_fk;
+  // `);
+  //
+  return db('users as u')
+    .join('orders as o', 'u.user_id', 'o.user_id_fk')
+    .select('o.total as total', 'u.username as username');
+  // .where('u.user_id', user_id);
 }
 
 // ==============================================
